c9b05b72278b51839bf6d1e0813e9c60bc9ce2bc
Kawatani Atsushi <katsushi61@gmail.com>
2023-07-22 01:11:12 +0900

    Dcokerファイル削除・pythonの実行ファイルの変更

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## 実行
 `docker compose exec python3 bash`  
 `python opt/sample.py`  
+
+`docker exec -it python3 bash`
+`python /root/opt/sample.py`
+
+
+https://di-acc2.com/system/rpa/10270/
--- opt/sample.py
+++ opt/sample.py
@@ -1,10 +1,41 @@
+# Selenium
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from time import sleep
+import time
 
-import math
-import sys
+# Chrome Driver
+from webdriver_manager.chrome import ChromeDriverManager
 
-def main():
-  val = float(sys.argv[1])
-  print(math.radians(val))
+# ブラウザインスタンス
+#browser = webdriver.Chrome(ChromeDriverManager().install())
 
-if __name__ == "__main__":
-  main()
+driver = webdriver.Chrome()
+driver.get('https://www.yahoo.co.jp/')
+
+time.sleep(2)
+element = driver.find_element(By.LINK_TEXT, "ファイナンス")
+element.click()
+time.sleep(5)
+driver.quit()
+
+
+# ブラウザを開く。
+# 対象のサイトに遷移する
+driver = webdriver.Chrome()
+driver.get('https://cha1ra.github.io/scrayping-handson/index.html')
+
+# 入力フォームを選択する
+el = driver.find_element(By.CSS_SELECTOR,'#id_text')
+# 入力フォーム内をリセットする
+el.clear()
+# 入力フォームに文字を入力する
+el.send_keys("test")
+# ボタンを選択する
+button = driver.find_element(By.CSS_SELECTOR,'button')
+# ボタンをクリックする
+button.click()
+# id=result にある文章を表示する
+print(driver.find_element(By.CSS_SELECTOR,'#result').text)
